a2a12d5d62f0820fc445a9a43cc2d11821c13082
iamrokibul <rootcoderboy@gmail.com>
2024-09-24 14:45:01 +0400

    log-reg-auth

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,44 @@ import News from '../News/News';
 
 const Home = () => {
     const [news, setNews] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
 
     useEffect(() => {
-        fetch('news.json')
-        .then(res => res.json())
-        .then(data => setNews(data));
+        // Fetch data from local API
+        const fetchData = async()=> {
+            const response = await fetch('news.json');
+            const data = await response.json();
+            setNews(data);
+        }
+        fetchData();
 
     },[]);
     // console.log(news);
 
+    // Logic to calculate the index of the items to show for the current page
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentData = news.slice(indexOfFirstItem, indexOfLastItem);
+
+    // Handler for changing the page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+
     return (
         <div className='p-3 w-full min-h-[50vh] border text-center'>
-            <h2 className='text-3xl mt-5 font-bold'>Welcome to Homepage {news.length}</h2>
+            <h2 className='text-5xl pb-5 font-bold border-b'>Welcome to Homepage</h2>
             {
-                news.map(a_news => <News key={a_news._id} a_news={a_news}></News>)
+                currentData.map(a_news => <News key={a_news._id} a_news={a_news}></News>)
             }
+            {/* Simple Pagination */}
+            <div className='mt-5'>
+                {Array.from({ length: Math.ceil(news.length / itemsPerPage) }, (_, index) => (
+                <button className='m-2 p-3 border inline-block' key={index} onClick={() => paginate(index + 1)}>
+                    {index + 1}
+                </button>
+                ))}
+            </div>
         </div>
     );
 };
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -4,7 +4,7 @@ const News = ({a_news}) => {
     const {title, image_url} = a_news;
     return (
         <div>
-            <h2 className='text-3xl text-white pt-5 pb-3'>{title}</h2>
+            <h2 className='text-3xl text-green-500 pt-5 pb-3'>{title}</h2>
             <img src={image_url} alt="" />
         </div>
     );
